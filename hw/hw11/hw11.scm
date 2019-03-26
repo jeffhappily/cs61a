6d476ca8e7198ec41b4b72d6f6d2452e0c7508a3
jeffhappily <goodboy96@live.com.my>
2019-03-27 07:58:01 +0800

    Solve q2 and q3

--- hw/hw11/hw11.scm
+++ hw/hw11/hw11.scm
@@ -6,11 +6,19 @@
 )
 
 (define (scale-stream s k)
-  'YOUR-CODE-HERE
+  (if (null? s) '()
+	  (cons-stream (* k (car s)) (scale-stream (cdr-stream s) k)))
 )
 
 (define (has-cycle s)
-  'YOUR-CODE-HERE
+  (define (has-cycle-rec s sr)
+	(if (null? sr) #f
+		(or
+		  (eq? (car s) (car sr))
+		  (has-cycle-rec s (cdr-stream sr))
+		  (has-cycle-rec (cdr-stream sr) (cdr-stream sr)))))
+
+  (has-cycle-rec s (cdr-stream s))
 )
 (define (has-cycle-constant s)
   'YOUR-CODE-HERE
