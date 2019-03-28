8b0c5e513ccefb7438d3794e946f6bdb6ebbb9b8
jeffhappily <goodboy96@live.com.my>
2019-03-28 11:42:57 +0800

    Revert "Finish hw 11"
    This reverts commit 7dc800b8f59c97797ef21094a4086ef44d35dd30.

--- hw/hw11/hw11.scm
+++ hw/hw11/hw11.scm
@@ -11,33 +11,15 @@
 )
 
 (define (has-cycle s)
-  ; tmp stores list of references to every element looped through
-  ; think of it like a pointers
-  ; 
-  (define (has-cycle-rec s tmp)
-	(cond 
-      ((null? s) #f)
-      ((contains tmp s) #t)
-      (else (has-cycle-rec (cdr-stream s) (cons s tmp)))))
-
-  (has-cycle-rec s nil)
-)
-
-(define (contains lst s)
-  (cond
-    ((null? lst) #f)
-    ((eq? (car lst) s) #t)
-    (else (contains (cdr lst) s))))
-
-
-(define (has-cycle-constant s)
   (define (has-cycle-rec s sr)
-    (cond
-      ((or
-         (null? sr)
-         (null? (cdr-stream sr))) #f)
-      ((or 
-         (eq? s sr)
-         (eq? s (cdr-stream sr))) #t)
-      (else (has-cycle-rec (cdr-stream s) (cdr-stream (cdr-stream sr))))))
-  (has-cycle-rec s (cdr-stream s)))
+	(if (null? sr) #f
+		(or
+		  (eq? (car s) (car sr))
+		  (has-cycle-rec s (cdr-stream sr))
+		  (has-cycle-rec (cdr-stream sr) (cdr-stream sr)))))
+
+  (has-cycle-rec s (cdr-stream s))
+)
+(define (has-cycle-constant s)
+  'YOUR-CODE-HERE
+)
