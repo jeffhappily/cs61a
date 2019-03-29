5cc65e15d0a9c202ae98c1ee03868b96177b291e
jeffhappily <goodboy96@live.com.my>
2019-03-29 09:38:04 +0800

    Finish until extra q7

--- lab/lab12/lab12_extra.sql
+++ lab/lab12/lab12_extra.sql
@@ -2,31 +2,30 @@
 
 -- Q5
 CREATE TABLE greatstudents AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.date, a.color, a.pet, a.number, b.number FROM students as a, fa17students as b WHERE a.date = b.date AND a.color = b.color AND a.pet = b.pet;
 
 -- Q6
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven FROM students as a, checkboxes as b WHERE a.time = b.time AND a.number = 7 AND b.'7' = 'True';
 
 -- Q7
 CREATE TABLE fa17favnum AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT number, COUNT(*) as count FROM fa17students GROUP BY number ORDER BY count DESC LIMIT 1;
 
 
 CREATE TABLE fa17favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT pet, COUNT(*) as count FROM fa17students GROUP BY pet ORDER BY count DESC LIMIT 10;
 
 CREATE TABLE sp18favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*) as count FROM students GROUP BY pet ORDER BY count DESC LIMIT 10;
 
 
 CREATE TABLE sp18dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "dog", COUNT(*) FROM students WHERE pet = 'dog';
 
 
 CREATE TABLE sp18alldogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "dog", COUNT(*) FROM students WHERE pet LIKE '%dog%';
 
 
 CREATE TABLE obedienceimages AS
