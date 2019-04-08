ef348227ff7a98cff4e1feb716adc4a4bd2f1d68
jeffhappily <goodboy96@live.com.my>
2019-04-08 08:23:25 +0800

    Finish lab13.scm:

--- lab/lab13/lab13.scm
+++ lab/lab13/lab13.scm
@@ -1,10 +1,16 @@
 ; Q1
 (define (compose-all funcs)
-  'YOUR-CODE-HERE
-  nil
+  (if (null? funcs)
+      (lambda (x) x)
+      (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x))))
 )
 
 ; Q2
 (define (tail-replicate x n)
-  'YOUR-CODE-HERE
+  (define (tail x n ls)
+    (if (= n 0) 
+      ls
+      (tail x (- n 1) (cons x ls))))
+
+  (tail x n '())
 )
