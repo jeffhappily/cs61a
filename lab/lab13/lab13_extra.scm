b63219294829ea5b70b7fccc82e6381bb876004c
jeffhappily <goodboy96@live.com.my>
2019-04-12 23:44:36 +0800

    Finish deep map

--- lab/lab13/lab13_extra.scm
+++ lab/lab13/lab13_extra.scm
@@ -33,8 +33,10 @@
 
 ; Q6
 (define (deep-map fn s)
-  'YOUR-CODE-HERE
-  nil
+  (cond
+    ((null? s) '())
+    ((list? (car s)) (cons (deep-map fn (car s)) (deep-map fn (cdr s))))
+    (else (cons (fn (car s)) (deep-map fn (cdr s)))))
 )
 
 ; Q7
